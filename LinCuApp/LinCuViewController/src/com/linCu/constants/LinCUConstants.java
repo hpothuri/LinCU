213cdabde9696c38fbd9836d847c349ce3bfce09
DileepKumarRongali <dileeprongaliprojects@gmail.com>
2017-04-17 00:26:59 +0530

    card request UI

---
+++ LinCuApp/LinCuViewController/src/com/linCu/constants/LinCUConstants.java
@@ -0,0 +1,14 @@
+package com.linCu.constants;
+
+public class LinCUConstants {
+    public static final String MSR = "MSR";
+    public static final String SUP = "SUP";
+    public static final String MAN_ADMIN = "MAN_ADMIN";
+    public static final String PREPAID_ADMIN = "PREPAID_ADMIN";
+    public static final String FCB = "FCB";
+    public static final String EMAIL_USER = "dbmstestmail@gmail.com";
+    public static final String EMAIL_PASSWORD = "dbmstestmail123";
+    public LinCUConstants() {
+        super();
+    }
+}
